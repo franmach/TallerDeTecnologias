5e2d23f8543e497afb66825f6df8b494e51b1981
franmach <franmach20@outlook.com>
2024-07-27 10:26:48 -0300

    primer commit

---
+++ ObligatorioTT/ObligatorioTT/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ObligatorioTT
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

3e394368de516663912fee9c777ba431d98e74f9
menge <leonardosuarezok@gmail.com>
2024-08-17 16:53:33 -0300

    Cap6, todo el detalle con boton de cerrar andando y apertura de detail

--- ObligatorioTT/ObligatorioTT/Controls/MovieInfoBox.xaml.cs
+++ ObligatorioTT/ObligatorioTT/Controls/MovieInfoBox.xaml.cs
@@ -1,17 +1,27 @@
 using ObligatorioTT.Models;
+using System.Windows.Input;
 namespace ObligatorioTT.Controls;
 
 public partial class MovieInfoBox : ContentView
 {
 	public static readonly BindableProperty MediaProperty =
 		BindableProperty.Create(nameof(Media), typeof(Media), typeof(MovieInfoBox), null);
+
+	public event EventHandler Closed;
 	public MovieInfoBox()
 	{
 		InitializeComponent();
-	}
+		ClosedCommand = new Command(ExecuteCloseCommand);
+
+    }
 	public Media Media
 	{
 		get => (Media)GetValue(MovieInfoBox.MediaProperty);
 		set => SetValue(MovieInfoBox.MediaProperty, value);
 	}
+    public ICommand ClosedCommand { get; private set; }
+    private void ExecuteCloseCommand() => Closed?.Invoke(this,EventArgs.Empty);
+
+	private void Button_Clicked(object sender, EventArgs e) =>
+		Closed?.Invoke(this,EventArgs.Empty);
 }
